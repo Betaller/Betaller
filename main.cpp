9cf11a146fe21018b3cdb7724035a4ec38ff51aa
wangminghua <wangminghua@xiaomi.com>
2024-08-01 16:12:32 +0800

    [feature]
    add main.cpp
    
    Signed-off-by: wangminghua <wangminghua@xiaomi.com>

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/* ************************************************************************
+> File Name:     main.cpp
+> Author:        程序员Carl
+> 微信公众号:    代码随想录
+> Created Time:  Thu Aug  1 16:11:20 2024
+> Description:   
+ ************************************************************************/
+#include<iostream>
+
+int main(){
+    std::cout<<"hello world\n";
+    std::cout<<"second line\n";
+    return 0;
+}
